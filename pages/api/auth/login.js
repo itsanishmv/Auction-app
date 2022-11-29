06d385f807f073472dc43e0b21f581d309fe9a29
guptavarun619 <guptavarun619@gmail.com>
2022-11-30 01:17:53 +0530

    Added Product Model and refactored login handler

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -3,16 +3,44 @@ import Users from "../../../models/user";
 import { generateJWT } from "../../../utility-functions/generateJWT";
 
 export default async function LoginHandler(req, res) {
-  await connectDB();
-  const { name, email, password } = req.body;
-  const findRegisteredUser = await Users.find({ email, password });
-  if (findRegisteredUser.length > 0) {
-    res.status(200).json({
-      message: "logged in",
-      email,
-      token: await generateJWT(email),
-    });
+  if (req.method === "POST") {
+    await connectDB();
+    const { name, email, password } = req.body;
+    const userDetails = await Users.findOne({ email });
+    if (userDetails !== null) {
+      if (userDetails.password === password) {
+        let response = {
+          status: "Success",
+          message: `You are logged in as ${email}`,
+          data: {
+            message: "logged in",
+            email,
+            token: await generateJWT(email),
+          },
+        };
+        res.status(200).json(response);
+      } else {
+        let response = {
+          status: "Fail",
+          message: `Wrong Password`,
+          error: "You've entered wrong password",
+        };
+        res.status(200).json(response);
+      }
+    } else {
+      let response = {
+        status: "Fail",
+        message: `${email} user not found`,
+        error: "Please signup",
+      };
+      res.status(400).json(response);
+    }
   } else {
-    res.json({ message: "please signup" });
+    let response = {
+      status: "Fail",
+      message: "Unsupported method",
+      error: `[${req.method}] is not supported on '${req.baseUrl}'`,
+    };
+    res.status(406).json(response);
   }
 }
